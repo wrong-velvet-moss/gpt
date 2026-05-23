7254f47915a14df6c39b12f39d5ad74e755dd7f8
wrong-velvet-moss <shengze65@gmail.com>
2026-05-23 14:48:15 +1000

    Update Activation functions (Math Foundations 2/6)

--- foundations/activations.py
+++ foundations/activations.py
@@ -3,7 +3,13 @@ from numpy.typing import NDArray
 
 
 class Solution:
-    
+    """
+    Activation functions are what give neural networks gives
+    the ability to learn complex patterns. Without them,
+    a neural network is just a series of linear transformations.
+    No matter how many layers you stack, the output would still
+    be a linear function of the input.
+    """
     def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
         # z is a 1D NumPy array
         # Formula: 1 / (1 + e^(-z))
@@ -12,6 +18,13 @@ class Solution:
             
 
     def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        """
+        ReLU replaced sigmoid because ReLU gradient is f'(x) = 1.
+        It does not suffer from the vanishing gradient problem in which
+        sigmoid suffers from.
+
+        sigmoid = vanishing gradient results to slow learning.
+        """
         # z is a 1D NumPy array
         # Formula: max(0, z) element-wise
         return np.maximum(0, z)
