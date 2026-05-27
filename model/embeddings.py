22a7ba8ba3947a3aa58a7577a9b150c46567807b
wrong-velvet-moss <shengze65@gmail.com>
2026-05-27 13:35:12 +1000

    Update Word embeddings (NLP 1/4)

--- model/embeddings.py
+++ model/embeddings.py
@@ -8,6 +8,7 @@ class Solution:
         # token_ids: 1D array of integer token IDs
         # Return the embedding vectors for the given token IDs
         # return np.round(your_answer, 5)
-        
-        return np.round(embeddings[token_ids], 5)
-      
+        look_up = [None] * len(token_ids)
+        for i, token_id in enumerate(token_ids):
+            look_up[i] = embeddings[token_id]
+        return np.round(np.array(look_up), 5)
